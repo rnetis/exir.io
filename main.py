d9712ae658e6497f4af6e872442302482dec85f4
Rayan kahoorzadeh <rynsm4rt@gmail.com>
2018-04-16 03:14:51 +0430

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import requests
+
+token = raw_input("Enter your token access> ")
+re = requests.get("https://api.exir.tech/v0/user", headers = {"Authorization": "Bearer " +  token}).json()
+print("Hello" + re["full_name"].encode("utf-8"))
+
+print("Email: " + re["email"])
+print("1 - Get balance")
+print("2 - Get price")
+print("3 - place order")
+print("4 - Cancel order")
+x = raw_input("Enter would do you want> ")
+
+price = requests.get("https://api.exir.tech/v0/ticker?symbol=btc").json()["ticker"]
+if x == "1":
+	z = requests.get("https://api.exir.tech/v0/user/balance", headers = {"Authorization": "Bearer " +  token}).json()
+	print("Bitcoin Balance : " + str(z["btc_balance"]))
+	print("Bitcoin avaliable : " + str(z["btc_available"]))
+	print("Bitcoin pending : " + str(z["btc_pending"]))
+	print("Money Balance : " + str(z["fiat_balance"]))
+	print("Money avaliable : " + str(z["fiat_available"]))
+	print("Your bitcoin in toman : " + str(z["btc_balance"] * price))
+elif x == "2":
+	price = requests.get("https://api.exir.tech/v0/ticker?symbol=btc").json()["ticker"]
+	print(price)
+elif x == "3":
+	while "true":
+		side = raw_input("Buy or Sell > ")
+		side = side.lower()
+		if side == "buy" or side == "sell":
+			break
+	size = raw_input("Amount of the order > ")
+	pri = raw_input("Enter your price(" + price + ") > ")
+	y = requests.post("https://api.exir.tech/v0/order", headers = {"Authorization": "Bearer " +  token}, data = {"symbol":"btc","side":side,"size":size,"type":"limit","price":pri}).json()
+	print("Your order status : " + y["status"])
+	print("Your order id : " + y["id"])
+elif x == "4":
+	sexy = raw_input("All or one >")
+	sexy = sexy.lower()
+	while "true":
+		if sexy == "all":
+			print("This object not complated!")
+		elif sexy == "one":
+			print("This object not complated!")
+		else:
+			print("This object not complated!")
+else:
+	print("Wrong number!")
